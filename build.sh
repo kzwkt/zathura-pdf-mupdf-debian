5a4c57a7b7d0b4c0837abaa1cb762cca006dd4c4
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-04-19 11:03:30 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ apt install -y ca-certificates
 echo "deb-src https://deb.debian.org/debian stable main" |  tee -a /etc/apt/sources.list
 apt update
 apt build-dep zathura mupdf -y
-apt install wget libmupdf-dev zathura-dev gcc meson ca-certificates  -y
+apt install wget libmupdf-dev zathura-dev gcc meson ca-certificates binutils  -y
 version=0.4.1
 echo $version > version 
 wget https://github.com/pwmt/zathura-pdf-mupdf/archive/refs/tags/$version.tar.gz
@@ -14,3 +14,4 @@ sed -i -e '/^[[:space:]]*tesseract/s/^[[:space:]]*\(tesseract\)/# \1/' -e '/^[[:
 meson setup build
 cd build
 ninja
+strip build/libpdf-mupdf.so
